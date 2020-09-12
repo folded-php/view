eeed5fd81eb9d537649e03b773eaf3fdbde51a0b
Khalyomede <khalyomede@gmail.com>
2020-09-12 13:39:42 +0200

    #1 add new method to always pass some data to a specific view (which fixes #1)

---
+++ src/addDataToView.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Folded;
+
+if (!function_exists("addDataToView")) {
+    /**
+     * Always pass the data provided to a specific view when it is rendered.
+     *
+     * @param string $viewPath The path to the view (you can use dot syntax).
+     * @param array  $data     The data to pass to the view as key-value pairs.
+     *
+     * @since 0.2.0
+     *
+     * @example
+     * addDataToView("layouts.base", [
+     *  "companyName" => "Folded",
+     *  "companySlogan" => "Constellation of packages for your web app.",
+     * ]);
+     */
+    function addDataToView(string $viewPath, array $data): void
+    {
+        View::addData($viewPath, $data);
+    }
+}
